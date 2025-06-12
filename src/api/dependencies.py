1e45b1ded33c569946d06948c494d21fa3ebad03
Aleksander <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-06-12 18:24:55 +0200

    Update dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -2,5 +2,6 @@ import httpx
 from fastapi import Request
 
 
+#TODO remove AsyncConnector from state
 def get_http_client(request: Request) -> httpx.AsyncClient:
     return request.app.state.http_client
